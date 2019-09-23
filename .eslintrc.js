22b9064d2ef7a6045d5f6da9b518d572ea05979f
igoist <493512132@qq.com>
2019-09-23 11:15:08 +0800

    Rules supplement

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,35 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es6': true
-    },
-    'extends': [
-        // 'eslint:recommended', // NO THIS LINE !!!
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:prettier/recommended',
-        'prettier/react',
-        'prettier/standard'
-    ],
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
-    },
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaFeatures': {
-            'jsx': true
-        },
-        'ecmaVersion': 2018,
-        'sourceType': 'module'
-    },
-    'plugins': [
-        'react',
-        '@typescript-eslint',
-        'prettier'
-    ],
-    'rules': {
-        'indent': [
-            'error',
-            2
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ],
-        'prettier/prettier': 'error'
-    }
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    // 'eslint:recommended', // NO THIS LINE !!!
+    'plugin:@typescript-eslint/eslint-recommended',
+    'prettier/react',
+    'plugin:prettier/recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    // ecmaFeatures: {
+    //   jsx: false
+    // },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['react-hooks', 'react', '@typescript-eslint', 'prettier'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'prettier/prettier': 'error',
+    '@typescript-eslint/class-name-casing': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  }
 };
